58f3b53e68a35c458449d24ee042849800b29b35
ojiehdavid5 <ojiehdavid5@gmail.com>
2025-06-16 12:53:12 +0100

    Implement password and token utilities for secure authentication

--- main.go
+++ main.go
@@ -1,11 +1,26 @@
 package main
+
 import (
 	"fmt"
+
 	"github.com/chuks/PAYBOTGO/bot"
+	"github.com/chuks/PAYBOTGO/config"
+	"github.com/gofiber/fiber/v2"
 )
 
-func main(){
-	 fmt.Println("chuks on chains")
-	 bot.StartBot()
-	 fmt.Println("Bot started")
+func main() {
+	config.ConnectDB()
+		bot.StartBot()
+	fmt.Println("Bot started")
+
+
+		app := fiber.New()
+
+	// // app.Post("/api/register", auth.Register)
+
+app.Listen(":3000")
+
+
+
+	// fmt.Println("chuks on chains")
 }
