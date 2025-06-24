f8daf02c817f38c3a46fbd396f9655937776bd82
ojiehdavid5 <ojiehdavid5@gmail.com>
2025-06-24 09:22:54 +0000

    Jun 24, 2025, 10:22 AM

--- bot/bot.go
+++ bot/bot.go
@@ -116,7 +116,6 @@ func StartBot() {
 				bot.Send(tgbotapi.NewMessage(chatID, "Enter your password:"))
 			case "awaiting_login_password":
 				session.Password = text
-				delete(userStates, chatID) // remove session
 
 				payload := map[string]any{
 					"email":    session.Email,
